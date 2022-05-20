03192057f7cad9dfc821e389a8def097bc05bff3
Hakan Guclu <creatorX64@gmail.com>
2022-05-20 03:33:21 +0300

    desktop nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,66 +6,67 @@ const Navigation = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
   return (
-    <nav className="relative flex items-center justify-between p-4">
-      {/* Logo */}
-      <Link href="/">
-        <a aria-label="Go to homepage">
-          <Image src="/logo.svg" alt="Website logo" width={93} height={19} />
-        </a>
-      </Link>
+    <nav className="relative p-4 md:py-11 md:px-20">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <Link href="/">
+          <a
+            aria-label="Go to homepage"
+            className="relative h-[19px] w-[93px] md:h-[29px] md:w-[139px]"
+          >
+            <Image src="/logo.svg" alt="Website logo" layout="fill" />
+          </a>
+        </Link>
 
-      {/* Hamburger icon */}
-      <span
-        className="cursor-pointer"
-        aria-label="Open navigation menu"
-        onClick={() => setIsMobileNavOpen(true)}
-      >
-        <Image
-          src="/hamburger.svg"
-          alt="Hamburger icon"
-          width={32}
-          height={32}
-        />
-      </span>
-
-      {/* Navigation menu */}
-      <div
-        className={`absolute top-0 left-0 z-50 grid h-screen w-full place-items-center border-l-2 border-slate-900 bg-slate-800 transition-transform duration-500 ${
-          isMobileNavOpen
-            ? "pointer-events-auto translate-x-0"
-            : "pointer-events-none translate-x-full"
-        }`}
-      >
+        {/* Hamburger icon */}
         <span
-          className="absolute top-4 right-5 cursor-pointer"
-          aria-label="Close navigation menu"
-          onClick={() => setIsMobileNavOpen(false)}
+          className="z-[100] cursor-pointer md:hidden"
+          aria-label="Toggle navigation menu"
+          onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
         >
-          <Image src="/close.svg" width={32} height={32} alt="Close icon" />
+          {isMobileNavOpen ? (
+            <Image src="/close.svg" width={32} height={32} alt="Close icon" />
+          ) : (
+            <Image
+              src="/hamburger.svg"
+              alt="Hamburger icon"
+              width={32}
+              height={32}
+            />
+          )}
         </span>
 
-        <ul className=" flex flex-col items-start justify-center gap-10 text-lg font-semibold text-slate-100">
-          <li>
-            <Link href="/#templates">
-              <a>Templates</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/license">
-              <a>License</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/#contact">
-              <a>Contact</a>
-            </Link>
-          </li>
-        </ul>
+        {/* Navigation menu */}
+        <div
+          className={`absolute top-0 left-0 z-50 grid h-screen w-full place-items-center bg-slate-800 transition-transform duration-500 md:static md:block md:h-max md:w-max md:bg-transparent ${
+            isMobileNavOpen
+              ? "pointer-events-auto translate-x-0"
+              : "pointer-events-none translate-x-full md:pointer-events-auto md:translate-x-0"
+          }`}
+        >
+          <ul className=" flex flex-col items-start justify-center gap-10 text-lg font-semibold text-slate-100 md:flex-row md:text-base">
+            <li>
+              <Link href="/#templates">
+                <a>Templates</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/license">
+                <a>License</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/#contact">
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
